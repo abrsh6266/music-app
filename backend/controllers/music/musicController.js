dc1918de9e846dd39926725e3f0c8538690aad81
Abrham <abrsh6266@gmail.com>
2024-08-29 01:46:13 -0800

    adding search functionality when getting musics

--- backend/controllers/music/musicController.js
+++ backend/controllers/music/musicController.js
@@ -34,14 +34,73 @@ exports.getMusics = asyncHandler(async (req, res) => {
   const limit = parseInt(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
-  const musics = await Music.find().skip(skip).limit(limit);
-  const total = await Music.countDocuments();
+  let query = {};
+  if (search) {
+    const searchRegex = new RegExp(search, "i");
+    query = {
+      $or: [
+        { title: searchRegex },
+        { artist: searchRegex },
+        { album: searchRegex },
+        { genre: searchRegex },
+      ],
+    };
+  }
+
+  const musics = await Music.find(query).skip(skip).limit(limit);
+  const total = await Music.countDocuments(query);
 
   res.json({
     status: "success",
     currentPage: page,
     totalPages: Math.ceil(total / limit),
-    totalItems: total,
+    totalMusic: total,
     musics,
+  });
+});
+
+// update music
+exports.updateMusic = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { title, genre, album, artist } = req.body;
+
+  const updatedMusic = await Music.findByIdAndUpdate(
+    id,
+    {
+      title,
+      genre,
+      album: album || "Unknown",
+      artist,
+    },
+    { new: true }
+  );
+
+  if (!updatedMusic) {
+    res.status(404);
+    throw new Error("Music not found");
+  }
+
+  res.json({
+    status: "success",
+    message: "Music successfully updated",
+    updatedMusic,
+  });
+});
+
+// delete music
+exports.deleteMusic = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const deletedMusic = await Music.findByIdAndDelete(id);
+
+  if (!deletedMusic) {
+    res.status(404);
+    throw new Error("Music not found");
+  }
+
+  res.json({
+    status: "success",
+    message: "Music successfully deleted",
+    deletedMusic,
   });
 });
--- backend/routes/music/musicRoute.js
+++ backend/routes/music/musicRoute.js
@@ -2,13 +2,24 @@ const multer = require("multer");
 const express = require("express");
 const storage = require("../../utils/fileUpload");
 const upload = multer({ storage });
-const { createMusic, getMusics } = require("../../controllers/music/musicController");
+const {
+  createMusic,
+  getMusics,
+  deleteMusic,
+  updateMusic,
+} = require("../../controllers/music/musicController");
 const musicRouter = express.Router();
 
-//create
+// create
 musicRouter.post("/", upload.single("file"), createMusic);
 
-// get musics
+//* get musics
 musicRouter.get("/", getMusics);
+
+//? Update existing music entry
+musicRouter.put("/:id", upload.single("file"), updateMusic);
+
+//! Delete a music entry
+musicRouter.delete("/:id", deleteMusic);
 
 module.exports = musicRouter;
